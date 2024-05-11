2d396a9696b5fc1483c39715a21e9ea82338dced
herortt <61754159+herott-ai@users.noreply.github.com>
2024-05-11 15:25:39 +0800

    Update buf.h
    just add comments

--- kernel/buf.h
+++ kernel/buf.h
@@ -1,8 +1,8 @@
 struct buf {
   int valid;   // has data been read from disk?
   int disk;    // does disk "own" buf?
-  uint dev;
-  uint blockno;
+  uint dev;    /// device
+  uint blockno; /// sector number
   struct sleeplock lock;
   uint refcnt;
   struct buf *prev; // LRU cache list
